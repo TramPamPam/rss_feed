c218ce6f9aedb42034b491e3d202f56f119fd363
bezpalchuk <bez_om@ukr.net>
2013-11-18 17:39:14 +0200

    rss from scratch 1 =)

--- src/com/example/HT2/CheckForUpdateService.java
+++ src/com/example/HT2/CheckForUpdateService.java
@@ -16,86 +16,9 @@ import android.util.Log;
 
 public class CheckForUpdateService extends Service {
 
-    NotificationManager notifManager;
-    JSONObject lastJson;
-
-    static final int TIMER_INTERVAL = 300000; //5m
-    static final int NOTIFICATION_ID = 12365897;
-
-    Handler timerHandler = new Handler();
-    Runnable timerRunnable = new Runnable() {
-        @Override
-        public void run() {
-            serviceTask();
-            timerHandler.postDelayed(this, TIMER_INTERVAL);
-        }
-    };
-
-    public void onCreate() {
-        super.onCreate();
-        notifManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        sendNotification("Service starting to work!", false);
-
-        Log.d("HW6", "onCreate");
-    }
-
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("HW6", "onStartCommand");
-//                serviceTask();
-        timerHandler.postDelayed(timerRunnable, 0);
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    public void onDestroy() {
-        timerHandler.removeCallbacks(timerRunnable);
-        notifManager.cancel(NOTIFICATION_ID);
-        super.onDestroy();
-        Log.d("HW6", "onDestroy");
-    }
 
     @Override
-    public IBinder onBind(Intent arg0) {
-        Log.d("HW6", "onBind");
-        return null;
-    }
-
-    void serviceTask() {
-        new Thread(new Runnable() {
-            public void run() {
-                JSONObject json = RSSActivity.getJSONFromUrl(RSSActivity.URL);
-                if (lastJson != null)
-                {
-                    if (json.hashCode() != lastJson.hashCode())
-                    {
-                        sendNotification("RSS Updated!", true);
-                        Log.d("HW6", "RSS Updated!");
-                    }
-                }
-                lastJson = json;
-
-//                                Log.d("HW6", "get json " + json);
-            }
-        }).start();
-    }
-
-    void sendNotification(String message, boolean isAnimated) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.location_web_site).setContentTitle("RSS Reader Check for update service").setContentText(message);
-
-        if (isAnimated)
-        {
-            mBuilder.setDefaults(Notification.DEFAULT_LIGHTS);
-        }
-
-        Intent resultIntent = new Intent(this, RSSActivity.class);
-
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-
-        stackBuilder.addParentStack(RSSActivity.class);
-
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setContentIntent(resultPendingIntent);
-
-        notifManager.notify(NOTIFICATION_ID, mBuilder.build());
+    public IBinder onBind(Intent intent) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
--- src/com/example/HT2/activities/RSSActivity.java
+++ src/com/example/HT2/activities/RSSActivity.java
@@ -1,234 +1,149 @@
+
 package com.example.HT2.activities;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import com.example.HT2.CheckForUpdateService;
+import android.view.animation.Animation;
+import android.widget.*;
 import com.example.HT2.FeedItem;
 import com.example.HT2.R;
-import com.example.HT2.RssListAdapter;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.*;
-import java.util.ArrayList;
 
-public class RSSActivity extends ActionBarActivity {
-    /**
-     * Called when the activity is first created.
-     */
-    private ArrayList<FeedItem> feedList = null;
-    private ProgressBar progressbar = null;
-    private ListView feedListView = null;
-    public static final String URL = "http://javatechig.com/api/get_category_posts/?dev=1&slug=android";
-    boolean mDualPane;
+public class RSSActivity extends ActionBarActivity
+        implements HeadlinesFragment.OnHeadlineSelectedListener {
 
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mPlanetTitles;
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_articles);
 
-        setContentView(R.layout.main_list);
-        progressbar = (ProgressBar) findViewById(R.id.progressBar);
+        getSupportActionBar().setTitle("RSS");
+        getSupportActionBar().setIcon(R.drawable.ic_action_social_person);
 
-        View detailsFrame = findViewById(R.id.details);
-        mDualPane = (detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE);
+        if (findViewById(R.id.fragment_container) != null) {
 
-        if (mDualPane){
-            detailsFrame.setVisibility(View.GONE);
-        }
-
-        new DownloadFilesTask().execute(URL);
-
-        startService(new Intent(this, CheckForUpdateService.class));
-
-        getSupportActionBar().setIcon( R.drawable.ic_action_social_person );
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item_refresh:
-                new DownloadFilesTask().execute(URL);
-                feedListView.setVisibility(View.GONE);
-                if (mDualPane) {
-                    findViewById(R.id.details).setVisibility(View.GONE);
-                }
-                progressbar.setVisibility(View.VISIBLE);
-                return true;
-            case R.id.item_stopService:
-                stopService(new Intent(this, CheckForUpdateService.class));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    public void updateList() {
-        feedListView= (ListView) findViewById(R.id.custom_list);
-        feedListView.setVisibility(View.VISIBLE);
-        if (mDualPane) {
-            findViewById(R.id.details).setVisibility(View.VISIBLE);
-        }
-        progressbar.setVisibility(View.GONE);
-
-        feedListView.setAdapter(new RssListAdapter(this, feedList));
-        feedListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> a, View v, int position,        long id) {
-
-                PerformListClick(position);
+            if (savedInstanceState != null) {
+                return;
             }
-        });
 
-        if (mDualPane) {
-            PerformListClick(0);
+            HeadlinesFragment firstFragment = new HeadlinesFragment();
+            firstFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
         }
     }
 
-    private void PerformListClick(int index) {
-        Object o = feedListView.getItemAtPosition(index);
-        FeedItem newsData = (FeedItem) o;
-        getSupportActionBar().setSubtitle(((FeedItem) o).getTitle());
+    public void onArticleSelected(int position) {
 
-        if (mDualPane) {
-            DetailsFragment details = new DetailsFragment();
 
-            Bundle args = new Bundle();
-            args.putSerializable("feed", newsData);
-            details.setArguments(args);
+        if(!isItATablet(getApplicationContext())){
 
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.details, details);
-            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-            ft.commit();
+            selectItem(position);
+
+            mTitle = mDrawerTitle = getTitle();
+            mPlanetTitles = getResources().getStringArray(R.array.game_menu);
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+            mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+// Set the adapter for the list view
+            mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                    R.layout.support_simple_spinner_dropdown_item, mPlanetTitles));
+
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+            mDrawerToggle = new ActionBarDrawerToggle(
+                    this,                  /* host Activity */
+                    mDrawerLayout,         /* DrawerLayout object */
+                    R.drawable.abc_ic_go,  /* nav drawer icon to replace 'Up' caret */
+                    R.string.drawer_open,  /* "open drawer" description */
+                    R.string.drawer_close  /* "close drawer" description */
+            ) {
+
+                /** Called when a drawer has settled in a completely closed state. */
+                public void onDrawerClosed(View view) {
+                    getSupportActionBar().setSubtitle(mTitle);
+                }
+
+                /** Called when a drawer has settled in a completely open state. */
+                public void onDrawerOpened(View drawerView) {
+                    getSupportActionBar().setSubtitle(mDrawerTitle);
+                }
+            };
+
+            // Set the drawer toggle as the DrawerListener
+            mDrawerLayout.setDrawerListener(mDrawerToggle);
+            // Set the list's click listener
+            mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+
         } else {
-            Intent intent = new Intent(RSSActivity.this, DetailsActionBarActivity.class);
-            intent.putExtra("feed", newsData);
-            startActivity(intent);
+
+            selectItemTablet(position);
         }
     }
 
-    private class DownloadFilesTask extends AsyncTask<String, Integer, Void> {
+   private void selectItemTablet(int position) {
+        Toast toast;
+        ArticleFragment articleFragment = new ArticleFragment();
 
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            if (feedList != null) {
-                updateList();
-            }
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-            String url = params[0];
-
-            // getting JSON string from URL
-            JSONObject json = getJSONFromUrl(url);
-
-            //parsing json data
-            parseJson(json);
-            return null;
-        }
-    }
-
-    static public JSONObject getJSONFromUrl(String url) {
-        InputStream is = null;
-        JSONObject jObj = null;
-        String json = null;
-
-        // Making HTTP request
-        try {
-            // defaultHttpClient
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
-
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            is = httpEntity.getContent();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    is, "iso-8859-1"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            is.close();
-            json = sb.toString();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            jObj = new JSONObject(json);
-        } catch (JSONException e) {
-            Log.e("JSON Parser", "Error parsing data " + e.toString());
-        }
-
-        // return JSON String
-        return jObj;
+        getSupportFragmentManager()
+               .beginTransaction()
+               .replace(R.id.article_fragment, articleFragment)
+               .addToBackStack(null)
+               .commit();
 
     }
 
-    public void parseJson(JSONObject json) {
-        try {
+   private void selectItem(int position) {
+       ArticleFragment articleFragment = new ArticleFragment();
 
-            // parsing json object
-            if (json.getString("status").equalsIgnoreCase("ok")) {
-                JSONArray posts = json.getJSONArray("posts");
+       getSupportFragmentManager()
+               .beginTransaction()
+               .replace(R.id.fragment_container, articleFragment)
+               .addToBackStack(null)
+               .commit();
 
-                feedList = new ArrayList<FeedItem>();
 
-                for (int i = 0; i < posts.length(); i++) {
-                    JSONObject post = (JSONObject) posts.getJSONObject(i);
-                    FeedItem item = new FeedItem();
-                    item.setTitle(post.getString("title"));
-                    item.setDate(post.getString("date"));
-                    item.setId(post.getString("id"));
-                    item.setUrl(post.getString("url"));
-                    item.setContent(post.getString("content"));
-                    JSONArray attachments = post.getJSONArray("attachments");
+   }
 
-                    if (null != attachments && attachments.length() > 0) {
-                        JSONObject attachment = attachments.getJSONObject(0);
-                        if (attachment != null)
-                            item.setAttachmentUrl(attachment.getString("url"));
-                    }
+    @Override
+    public void onBackPressed() {
+        FragmentManager fm = getSupportFragmentManager();
+        fm.popBackStack();
+        getSupportActionBar().setSubtitle("");
 
-                    feedList.add(item);
-                }
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
+    }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
         }
+    }
+
+    public boolean isItATablet(Context context) {
+        boolean xlarge = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == 4);
+        boolean large = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
+        return (xlarge || large);
     }
 }
